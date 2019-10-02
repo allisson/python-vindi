96dc5c59a7a83f6295248916cd07d71f10692367
Allisson Azevedo <allisson@gmail.com>
2019-10-02 18:45:45 -0300

    Versão 1.0.0

--- vindi/api.py
+++ vindi/api.py
@@ -4,85 +4,63 @@ from simple_rest_client.api import API
 from vindi import resource
 
 
-def get_api_instance(token='', api_root_url=None, timeout=3):
+def get_api_instance(token="", api_root_url=None, timeout=3):
     headers = {
-        'Authorization': basic_http_auth(token, ''),
-        'Content-Type': 'application/json'
+        "Authorization": basic_http_auth(token, ""),
+        "Content-Type": "application/json",
     }
     import_batches_headers = {
-        'Authorization': basic_http_auth(token, ''),
-        'Content-Type': 'multipart/form-data'
+        "Authorization": basic_http_auth(token, ""),
+        "Content-Type": "multipart/form-data",
     }
-    api_root_url = api_root_url or 'https://app.vindi.com.br/api/v1/'
+    api_root_url = api_root_url or "https://app.vindi.com.br/api/v1/"
     api = API(
-        api_root_url=api_root_url, headers=headers, json_encode_body=True,
-        timeout=timeout
+        api_root_url=api_root_url,
+        headers=headers,
+        json_encode_body=True,
+        timeout=timeout,
     )
     api.add_resource(
-        resource_name='customers', resource_class=resource.CustomerResource
+        resource_name="customers", resource_class=resource.CustomerResource
+    )
+    api.add_resource(resource_name="plans", resource_class=resource.PlanResource)
+    api.add_resource(resource_name="products", resource_class=resource.ProductResource)
+    api.add_resource(
+        resource_name="payment_methods", resource_class=resource.PaymentMethodResource
     )
     api.add_resource(
-        resource_name='plans', resource_class=resource.PlanResource
+        resource_name="discounts", resource_class=resource.DiscountResource
     )
     api.add_resource(
-        resource_name='products', resource_class=resource.ProductResource
+        resource_name="subscriptions", resource_class=resource.SubscriptionResource
     )
     api.add_resource(
-        resource_name='payment_methods',
-        resource_class=resource.PaymentMethodResource
+        resource_name="product_items", resource_class=resource.ProductItemResource
+    )
+    api.add_resource(resource_name="periods", resource_class=resource.PeriodResource)
+    api.add_resource(resource_name="bills", resource_class=resource.BillResource)
+    api.add_resource(
+        resource_name="bill_items", resource_class=resource.BillItemResource
+    )
+    api.add_resource(resource_name="charges", resource_class=resource.ChargeResource)
+    api.add_resource(
+        resource_name="transactions", resource_class=resource.TransactionResource
     )
     api.add_resource(
-        resource_name='discounts', resource_class=resource.DiscountResource
+        resource_name="payment_profiles", resource_class=resource.PaymentProfileResource
     )
+    api.add_resource(resource_name="usages", resource_class=resource.UsageResource)
+    api.add_resource(resource_name="invoices", resource_class=resource.InvoiceResource)
     api.add_resource(
-        resource_name='subscriptions',
-        resource_class=resource.SubscriptionResource
+        resource_name="movements", resource_class=resource.MovementResource
     )
+    api.add_resource(resource_name="messages", resource_class=resource.MessageResource)
     api.add_resource(
-        resource_name='product_items',
-        resource_class=resource.ProductItemResource
-    )
-    api.add_resource(
-        resource_name='periods', resource_class=resource.PeriodResource
-    )
-    api.add_resource(
-        resource_name='bills', resource_class=resource.BillResource
-    )
-    api.add_resource(
-        resource_name='bill_items', resource_class=resource.BillItemResource
-    )
-    api.add_resource(
-        resource_name='charges', resource_class=resource.ChargeResource
-    )
-    api.add_resource(
-        resource_name='transactions',
-        resource_class=resource.TransactionResource
-    )
-    api.add_resource(
-        resource_name='payment_profiles',
-        resource_class=resource.PaymentProfileResource
-    )
-    api.add_resource(
-        resource_name='usages', resource_class=resource.UsageResource
-    )
-    api.add_resource(
-        resource_name='invoices', resource_class=resource.InvoiceResource
-    )
-    api.add_resource(
-        resource_name='movements', resource_class=resource.MovementResource
-    )
-    api.add_resource(
-        resource_name='messages', resource_class=resource.MessageResource
-    )
-    api.add_resource(
-        resource_name='import_batches',
+        resource_name="import_batches",
         resource_class=resource.ImportBatchResource,
-        json_encode_body=False, headers=import_batches_headers
+        json_encode_body=False,
+        headers=import_batches_headers,
     )
-    api.add_resource(
-        resource_name='merchant', resource_class=resource.MerchantResource
-    )
-    api.add_resource(
-        resource_name='issues', resource_class=resource.IssueResource
-    )
+    api.add_resource(resource_name="merchant", resource_class=resource.MerchantResource)
+    api.add_resource(resource_name="issues", resource_class=resource.IssueResource)
     return api
--- vindi/resource.py
+++ vindi/resource.py
@@ -3,403 +3,169 @@ from simple_rest_client.resource import Resource
 
 class CustomerResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'customers'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'customers'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'customers/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'customers/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'customers/{}',
-        },
+        "list": {"method": "GET", "url": "customers"},
+        "create": {"method": "POST", "url": "customers"},
+        "retrieve": {"method": "GET", "url": "customers/{}"},
+        "update": {"method": "PUT", "url": "customers/{}"},
+        "destroy": {"method": "DELETE", "url": "customers/{}"},
     }
 
 
 class PlanResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'plans'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'plans'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'plans/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'plans/{}',
-        }
+        "list": {"method": "GET", "url": "plans"},
+        "create": {"method": "POST", "url": "plans"},
+        "retrieve": {"method": "GET", "url": "plans/{}"},
+        "update": {"method": "PUT", "url": "plans/{}"},
     }
 
 
 class ProductResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'products'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'products'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'products/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'products/{}',
-        }
+        "list": {"method": "GET", "url": "products"},
+        "create": {"method": "POST", "url": "products"},
+        "retrieve": {"method": "GET", "url": "products/{}"},
+        "update": {"method": "PUT", "url": "products/{}"},
     }
 
 
 class PaymentMethodResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'payment_methods'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'payment_methods/{}',
-        }
+        "list": {"method": "GET", "url": "payment_methods"},
+        "retrieve": {"method": "GET", "url": "payment_methods/{}"},
     }
 
 
 class DiscountResource(Resource):
     actions = {
-        'create': {
-            'method': 'POST',
-            'url': 'discounts'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'discounts/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'discounts/{}',
-        },
+        "create": {"method": "POST", "url": "discounts"},
+        "retrieve": {"method": "GET", "url": "discounts/{}"},
+        "destroy": {"method": "DELETE", "url": "discounts/{}"},
     }
 
 
 class SubscriptionResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'subscriptions'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'subscriptions'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'subscriptions/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'subscriptions/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'subscriptions/{}',
-        },
-        'renew': {
-            'method': 'POST',
-            'url': 'subscriptions/{}/renew',
-        },
-        'reactivate': {
-            'method': 'POST',
-            'url': 'subscriptions/{}/reactivate',
-        },
+        "list": {"method": "GET", "url": "subscriptions"},
+        "create": {"method": "POST", "url": "subscriptions"},
+        "retrieve": {"method": "GET", "url": "subscriptions/{}"},
+        "update": {"method": "PUT", "url": "subscriptions/{}"},
+        "destroy": {"method": "DELETE", "url": "subscriptions/{}"},
+        "renew": {"method": "POST", "url": "subscriptions/{}/renew"},
+        "reactivate": {"method": "POST", "url": "subscriptions/{}/reactivate"},
     }
 
 
 class ProductItemResource(Resource):
     actions = {
-        'create': {
-            'method': 'POST',
-            'url': 'product_items'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'product_items/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'product_items/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'product_items/{}',
-        },
+        "create": {"method": "POST", "url": "product_items"},
+        "retrieve": {"method": "GET", "url": "product_items/{}"},
+        "update": {"method": "PUT", "url": "product_items/{}"},
+        "destroy": {"method": "DELETE", "url": "product_items/{}"},
     }
 
 
 class PeriodResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'periods'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'periods/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'periods/{}',
-        },
-        'bill': {
-            'method': 'POST',
-            'url': 'periods/{}/bill',
-        },
+        "list": {"method": "GET", "url": "periods"},
+        "retrieve": {"method": "GET", "url": "periods/{}"},
+        "update": {"method": "PUT", "url": "periods/{}"},
+        "bill": {"method": "POST", "url": "periods/{}/bill"},
     }
 
 
 class BillResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'bills'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'bills'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'bills/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'bills/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'bills/{}',
-        },
-        'approve': {
-            'method': 'POST',
-            'url': 'bills/{}/approve',
-        },
-        'charge': {
-            'method': 'POST',
-            'url': 'bills/{}/charge',
-        },
+        "list": {"method": "GET", "url": "bills"},
+        "create": {"method": "POST", "url": "bills"},
+        "retrieve": {"method": "GET", "url": "bills/{}"},
+        "update": {"method": "PUT", "url": "bills/{}"},
+        "destroy": {"method": "DELETE", "url": "bills/{}"},
+        "approve": {"method": "POST", "url": "bills/{}/approve"},
+        "charge": {"method": "POST", "url": "bills/{}/charge"},
     }
 
 
 class BillItemResource(Resource):
-    actions = {
-        'retrieve': {
-            'method': 'GET',
-            'url': 'bill_items/{}',
-        },
-    }
+    actions = {"retrieve": {"method": "GET", "url": "bill_items/{}"}}
 
 
 class ChargeResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'charges'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'charges/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'charges/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'charges/{}',
-        },
-        'reissue': {
-            'method': 'POST',
-            'url': 'charges/{}/reissue',
-        },
-        'charge': {
-            'method': 'POST',
-            'url': 'charges/{}/charge',
-        },
-        'refund': {
-            'method': 'POST',
-            'url': 'charges/{}/refund',
-        },
-        'fraud_review': {
-            'method': 'POST',
-            'url': 'charges/{}/fraud_review',
-        },
+        "list": {"method": "GET", "url": "charges"},
+        "retrieve": {"method": "GET", "url": "charges/{}"},
+        "update": {"method": "PUT", "url": "charges/{}"},
+        "destroy": {"method": "DELETE", "url": "charges/{}"},
+        "reissue": {"method": "POST", "url": "charges/{}/reissue"},
+        "charge": {"method": "POST", "url": "charges/{}/charge"},
+        "refund": {"method": "POST", "url": "charges/{}/refund"},
+        "fraud_review": {"method": "POST", "url": "charges/{}/fraud_review"},
     }
 
 
 class TransactionResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'transactions'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'transactions'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'transactions/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'transactions/{}',
-        },
+        "list": {"method": "GET", "url": "transactions"},
+        "create": {"method": "POST", "url": "transactions"},
+        "retrieve": {"method": "GET", "url": "transactions/{}"},
+        "update": {"method": "PUT", "url": "transactions/{}"},
     }
 
 
 class PaymentProfileResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'payment_profiles'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'payment_profiles'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'payment_profiles/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'payment_profiles/{}',
-        },
-        'verify': {
-            'method': 'POST',
-            'url': 'payment_profiles/{}/verify',
-        },
+        "list": {"method": "GET", "url": "payment_profiles"},
+        "create": {"method": "POST", "url": "payment_profiles"},
+        "retrieve": {"method": "GET", "url": "payment_profiles/{}"},
+        "destroy": {"method": "DELETE", "url": "payment_profiles/{}"},
+        "verify": {"method": "POST", "url": "payment_profiles/{}/verify"},
     }
 
 
 class UsageResource(Resource):
     actions = {
-        'create': {
-            'method': 'POST',
-            'url': 'usages'
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'usages/{}',
-        },
+        "create": {"method": "POST", "url": "usages"},
+        "destroy": {"method": "DELETE", "url": "usages/{}"},
     }
 
 
 class InvoiceResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'invoices'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'invoices'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'invoices/{}',
-        },
-        'destroy': {
-            'method': 'DELETE',
-            'url': 'invoices/{}',
-        },
-        'retry': {
-            'method': 'POST',
-            'url': 'invoices/{}/retry',
-        },
+        "list": {"method": "GET", "url": "invoices"},
+        "create": {"method": "POST", "url": "invoices"},
+        "retrieve": {"method": "GET", "url": "invoices/{}"},
+        "destroy": {"method": "DELETE", "url": "invoices/{}"},
+        "retry": {"method": "POST", "url": "invoices/{}/retry"},
     }
 
 
 class MovementResource(Resource):
-    actions = {
-        'create': {
-            'method': 'POST',
-            'url': 'movements'
-        },
-    }
+    actions = {"create": {"method": "POST", "url": "movements"}}
 
 
 class MessageResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'messages'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'messages'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'messages/{}',
-        },
+        "list": {"method": "GET", "url": "messages"},
+        "create": {"method": "POST", "url": "messages"},
+        "retrieve": {"method": "GET", "url": "messages/{}"},
     }
 
 
 class ImportBatchResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'import_batches'
-        },
-        'create': {
-            'method': 'POST',
-            'url': 'import_batches'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'import_batches/{}',
-        },
+        "list": {"method": "GET", "url": "import_batches"},
+        "create": {"method": "POST", "url": "import_batches"},
+        "retrieve": {"method": "GET", "url": "import_batches/{}"},
     }
 
 
 class MerchantResource(Resource):
-    actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'merchant'
-        },
-    }
+    actions = {"list": {"method": "GET", "url": "merchant"}}
 
 
 class IssueResource(Resource):
     actions = {
-        'list': {
-            'method': 'GET',
-            'url': 'issues'
-        },
-        'retrieve': {
-            'method': 'GET',
-            'url': 'issues/{}',
-        },
-        'update': {
-            'method': 'PUT',
-            'url': 'issues/{}',
-        },
+        "list": {"method": "GET", "url": "issues"},
+        "retrieve": {"method": "GET", "url": "issues/{}"},
+        "update": {"method": "PUT", "url": "issues/{}"},
     }
